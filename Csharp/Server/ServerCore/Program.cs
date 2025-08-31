385b9d28ed42919b16fb8cb9eb8ed0fad8cee652
munjh61 <junho9661@gmail.com>
2025-09-01 06:59:51 +0900

    csharp

---
+++ Csharp/Server/ServerCore/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ServerCore
+{
+    class Program
+    {
+        static ThreadLocal<string> ThreadName = new ThreadLocal<string>(() => { return $"My Name Is {Thread.CurrentThread.ManagedThreadId}"; });
+        static void WhoAmI()
+        {
+            bool repeat = ThreadName.IsValueCreated;
+            if(repeat)
+                Console.WriteLine(ThreadName.Value + "(repeat)");
+            else 
+                Console.WriteLine(ThreadName.Value);
+        }
+        static void Main(string[] args)
+        {
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(3, 3);
+            // task 쉽게 생성
+            Parallel.Invoke(WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI);
+            ThreadName.Dispose();
+        }
+    }
+}
