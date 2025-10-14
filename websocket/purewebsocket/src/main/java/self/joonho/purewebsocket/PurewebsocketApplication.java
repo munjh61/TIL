4d8531432275a7f099199eae80aed7a9a8d2e20f
Joonho <junho9661@gmail.com>
2025-10-14 17:51:01 +0900

    a

---
+++ websocket/purewebsocket/src/main/java/self/joonho/purewebsocket/PurewebsocketApplication.java
@@ -0,0 +1,13 @@
+package self.joonho.purewebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurewebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurewebsocketApplication.class, args);
+	}
+
+}
---
+++ websocket/purewebsocket/src/test/java/self/joonho/purewebsocket/PurewebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package self.joonho.purewebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurewebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
